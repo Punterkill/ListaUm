4b98ba5cf70e1797059a5b693dd5749a355c071a
Tiago Pereira Salvino <168425343+Punterkill@users.noreply.github.com>
2024-05-15 10:44:41 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,41 @@
+public class Cachorro {
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	public Cachorro(String novoNome, String novaRaca, int novaIdade) {
+		this.nome = novoNome;
+		this.raca = novaRaca;
+		this.idade = novaIdade;
+	}
+	
+//get
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	
+//set
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setRaca(String novaRaca) {
+		this.raca = novaRaca;
+	}
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	
+	public String latir() {
+		return "Au au!";
+	}
+	
+
+}
