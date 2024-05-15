6705d0cce463d7d6b27419947eb7c1249f2d1e85
Tiago Pereira Salvino <168425343+Punterkill@users.noreply.github.com>
2024-05-15 10:55:00 -0300

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,36 @@
+public class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	public Data(int novoDia, int novoMes, int novoAno) {
+		this.dia = novoDia;
+		this.mes = novoMes;
+		this.ano = novoAno;
+	}
+	
+//get
+	
+	public int getDia() {
+		return dia;
+	}
+	public int getMes() {
+		return mes;
+	}
+	public int getAno() {
+		return ano;
+	}
+	
+//set
+	
+	public void setDia(int novoDia) {
+		this.dia = novoDia;
+	}
+	public void setMes(int novoMes) {
+		this.mes = novoMes;
+	}
+	public void setAno(int novoAno) {
+		this.ano = novoAno;
+		
+	}
+}
